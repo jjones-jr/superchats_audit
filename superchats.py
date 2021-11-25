41ea7bc4f6c17d09b1b924ab061daaf3e03f93ea
James Jones, Jr <2156057+jjones-jr@users.noreply.github.com>
2021-11-25 09:36:30 +0900

    commenting and converting print statements to console.print
    just cleaning up a bit.

--- superchats.py
+++ superchats.py
@@ -1,61 +1,53 @@
 from rich import print
 from rich.console import Console
+
 import re
 import pandas as pd
 import numpy as np
-import typer
+
 import os
 from io import StringIO
 import datetime
+
+import typer                #user interface library
 
 pd.set_option('display.max_colwidth', None)   #Show full columns and Rows
 pd.set_option('display.max_rows', None)
 
 console = Console()
-
 app = typer.Typer()
 
 @app.command()
-def mainloop(filename: str):
-    textfile = open(filename, 'r')
-    filetext = textfile.read()
-    textfile.close()
+def mainloop(filename: str):     #read in file from command line
+    textfile = open(filename, 'r')  
+    filetext = textfile.read()   #dump contents of the file into the filetext variable
+    textfile.close()             #close the file
     matches = re.findall(r"(.+) ([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]),[\s]([0-9][0-9]:[0-9][0-9])([\s][\S]*)([\s][\S]*)[|▶ ]Go to:([\s][\S]*)\n([Donated:]+) ([$]\d+.\d+)", filetext)
     df = pd.DataFrame(matches)   # df.rename(columns={"A": "a", "B": "c"})
-    df[8] = df[1] + ' ' + df[2] # concatenate Date and Time fields before conversion to Datetime type.
+    df[8] = df[1] + ' ' + df[2]  # concatenate Date and Time fields before conversion to Datetime type.
     df[8] = pd.to_datetime(df[8])
-    #df[5] = df[1] + ' ' + df[5] # concatenate Date and Time fields before conversion to Datetime type.
-    #print(df.info())
-
-    df[7] = df[7].replace({'\$':''}, regex = True)   #**Remove the '$'
-    # convert Superchats column to Float datatype
-    df[7] = pd.to_numeric(df[7])
-    del df[1]
+    df[7] = df[7].replace({'\$':''}, regex = True) #Remove the '$' from the Superchat amount column
+    df[7] = pd.to_numeric(df[7])  # convert Superchats column to the Float datatype to accommodate the decimal value
+    del df[1]                     # delete columns 1,2,3,4 and 6.
     del df[2]
     del df[3]
     del df[4]
     del df[6]
-    df = df.rename(columns={0:'Youtube_ID',5:'Video_Location_Time',7:'Superchat',8:'Timestamp'})
-
-    #https://www.geeksforgeeks.org/convert-the-column-type-from-string-to-datetime-format-in-pandas-dataframe/
-    print("........................INDIVIDUAL SUPERCHATS BY TIMESTAMP.....................................")
+    df = df.rename(columns={0:'Youtube_ID',5:'Video_Location_Time',7:'Superchat',8:'Timestamp'})  #name the columns
+    console.print("........................INDIVIDUAL SUPERCHATS BY TIMESTAMP.....................................",style="white")
     console.print("\n", df.round(2), "\n")
     #print(df['Superchat'].describe())
-    
-    print("........................BIGMAN7917's STATISTICS.................................................\n")
+    console.print("........................BIGMAN7917's STATISTICS.................................................\n", style="cyan")
     if df[(df['Youtube_ID'] == 'BIGMAN7917') & (df['Superchat'] >= 1)].empty:
-        print("..........................")
         console.print("[bold]ZERO[/] donations from [cyan][bold]BIGMAN7917[/][/]", style="bold red")
-    
-        print("..............................\n")
     else:    
-        print(df[(df['Youtube_ID'] == 'BIGMAN7917') & (df['Superchat'] >= 1)].round(2), "\n")  # ['Superchat'].sum() for total.
-        print(df[(df['Youtube_ID'] == 'BIGMAN7917')]['Superchat'].describe().round(2), "\n")   #about Bigman's donations
+        console.print(df[(df['Youtube_ID'] == 'BIGMAN7917') & (df['Superchat'] >= 1)].round(2), "\n")  # ['Superchat'].sum() for total.
+        console.print(df[(df['Youtube_ID'] == 'BIGMAN7917')]['Superchat'].describe().round(2), "\n")   #about Bigman's donations
     #together = [df, df2]
     #df3 = pd.concat(together)
-    print("SUPERCHATS BY USER_ID\n")
-    print(df.groupby(['Youtube_ID'])['Superchat'].sum().round(2), "\n")
-    print("TOTAL SUPERCHATS:  ", df['Superchat'].sum().round(2), "\n")
+    console.print("SUPERCHATS BY USER_ID\n")
+    console.print(df.groupby(['Youtube_ID'])['Superchat'].sum().round(2), "\n")
+    console.print("TOTAL SUPERCHATS:  ", df['Superchat'].sum().round(2), "\n")
     df.to_excel(filename + ".xlsx")
     df.to_xml(filename + ".xml")
 
